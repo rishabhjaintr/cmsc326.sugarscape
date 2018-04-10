e3e22a3d15200a80c34b98f9124b4ac6464ad27c
David Link <david.link@richmond.edu>
2018-04-10 15:38:07 -0400

    updated Agent.java with death scheduling
    schedule a death in movement method

--- Agent.java
+++ Agent.java
@@ -1,243 +1,249 @@
-//find unoccuppied cell with highest currentRes
+// find unoccuppied cell with highest currentRes
 
-//row - FOV + NUM_ROWS % NUM_ROWS for donut view. Same for column.
+// row - FOV + NUM_ROWS % NUM_ROWS for donut view. Same for column.
 public class Agent
 {
- private String id;   // identifier for the agent
- private int row;
- private int col;
- private int vision; // how many cells in any one direction can be seen
- private double wealth;   
- private double metRate;
- private int maxAge;
+    private String id;   // identifier for the agent
+    private int row;
+    private int col;
+    private int vision; // how many cells in any one direction can be seen
+    private double wealth;   
+    private double metRate;
+    private int maxAge;
 
- private double t_move;
- private double t_death;
+    private double t_move;
+    private double t_death;
 
- protected static Landscape landscape = null;
- protected static int NUM_ROWS;
- protected static int NUM_COLS;
- 
- public Agent(String id)
- {
-     this.id = id;
- }
+    protected static Landscape landscape = null;
+    protected static int NUM_ROWS;
+    protected static int NUM_COLS;
+    
+    public Agent(String id)
+    {
+        this.id = id;
+    }
 
- // simple accessor methods below
- public int getRow()
- {
-     return this.row; 
- }
- public int getCol()
- {
-     return this.col; 
- }
- public String getID()  
- {
-     return this.id;  
- }
- public int getVision() 
- {
-     return this.vision; 
- }
- public double getMetRate()
- {
-     return this.metRate;
- }
+    // simple accessor methods below
+    public int getRow()
+    {
+        return this.row; 
+    }
+    public int getCol()
+    {
+        return this.col; 
+    }
+    public String getID()  
+    {
+        return this.id;  
+    }
+    public int getVision() 
+    {
+        return this.vision; 
+    }
+    public double getMetRate()
+    {
+        return this.metRate;
+    }
 
- public double getMoveTime()
- {
-     return this.t_move;
- }
+    public double getMoveTime()
+    {
+        return this.t_move;
+    }
 
- public double getDeathTime()
- {
-     return this.t_death;
- }
+    public double getDeathTime()
+    {
+        return this.t_death;
+    }
 
- // simple mutator methods below
- public void setRowCol(int row, int col)
- {
-     this.row = row;
-     this.col = col;
- }
- public void setVision(int n)
- {
-     this.vision = n;
- }
- public void setMetRate(double n)
- {
-     this.metRate = n;
- }
+    // simple mutator methods below
+    public void setRowCol(int row, int col)
+    {
+        this.row = row;
+        this.col = col;
+    }
+    public void setVision(int n)
+    {
+        this.vision = n;
+    }
+    public void setMetRate(double n)
+    {
+        this.metRate = n;
+    }
 
- public void setMoveTime(double m)
- {
-     this.t_move = m;
- }
+    public void setMoveTime(double m)
+    {
+        this.t_move = m;
+    }
 
- public void setDeathTime(double m)
- {
-     this.t_death = m;
- }
+    public void setDeathTime(double m)
+    {
+        this.t_death = m;
+    }
 
+   
+    
+    public Cell movement(double t)
+    {
+        Cell nextCell = findNextCell(this.row, this.col, this.vision);
+        // set current cell to unoccupied before movement
+        landscape.getCellAt(this.row, this.col).setOccupied(false);
 
- 
- public Cell movement(double t)
- {
-     Cell nextCell = findNextCell(this.row, this.col, this.vision, t);
-     // set current cell to unoccupied before movement
-     landscape.getCellAt(this.row, this.col).setOccupied(false);
+        // assign new coordinates to agent and set the new cell to occupied
+        this.row = nextCell.row;
+        this.col = nextCell.col;
+        nextCell.setOccupied(true);
 
-     // assign new coordinates to agent and set the new cell to occupied
-     this.row = nextCell.row;
-     this.col = nextCell.col;
-     nextCell.setOccupied(true);
+        double tnext = t + this.getInterMove();
 
-     double tnext = t + this.getInterMove();
+        double nextWealth = computeWealth(t, tnext);
 
-     double nextWealth = computeWealth(t, tnext);
+        if(nextWealth > 0)
+        {
+            // live to see another move
+            this.setMoveTime(tnext);
+        }
+        else if(nextWealth <= 0)
+        {
+            // schedule a death at the moment the wealth dropped below 0 
+            // compute slope
+            double slope_m = (this.wealth - nextWealth)/(t - tnext);
+            // compute y-intercept
+            double c = this.wealth - (slope_m * t);
+            // compute time of death
+            double nextDeath = ((-1)*c) / slope_m;
+            this.setDeathTime(nextDeath);
+        }
 
-     if(nextWealth > 0)
-     {
-         // live to see another move
-         this.setMoveTime(tnext);
-     }
-     else if(nextWealth <= 0)
-     {
-         // schedule a death at the moment the wealth dropped below 0 (linear)
-     }
+        return nextCell;
+    }
 
-     return nextCell;
- }
+    private double getInterMove()
+    {
+        // exponential(rate = 1)
+    }
 
- private double getInterMove()
- {
-     // exponential(rate = 1)
-	 return 0;
- }
+    private double computeWealth(double tnow, double tnext)
+    {
+        Cell currentCell = landscape.getCellAt(this.row, this.col);
+        double gathered = (tnext - tnow) * currentCell.getRegrowthRate();
+        double consumed = (tnext - tnow) * this.metRate;
 
- private double computeWealth(double tnow, double tnext)
- {
-     Cell currentCell = landscape.getCellAt(this.row, this.col);
-     double gathered = (tnext - tnow) * currentCell.getRegrowthRate();
-     double consumed = (tnext - tnow) * this.metRate;
+        return (this.wealth + gathered - consumed);
+    }
 
-     return (this.wealth + gathered - consumed);
- }
+    private Cell findNextCell(int row, int col, int vision, double t)
+    {
+        Cell current;
+        Cell nextCell = null;
+        double cellDist = 0;
 
- private Cell findNextCell(int row, int col, int vision, double t)
- {
-     Cell current;
-     Cell nextCell = null;
-     double cellDist = 0;
+        // check up
+        for (int i = 1; i <= vision; i++)
+        {
+            // landscape???
+            int nextRow = (row - vision + NUM_ROWS - i) % NUM_ROWS;
+            current = landscape.getCellAt(nextRow, col);
 
-     // check up
-     for (int i = 1; i <= vision; i++)
-     {
-         // landscape???
-         int nextRow = (row - vision + NUM_ROWS - i) % NUM_ROWS;
-         current = landscape.getCellAt(nextRow, col);
+            if(current.getOccupied() == true)
+            {
+                continue;
+            }
 
-         if(current.getOccupied() == true)
-         {
-             continue;
-         }
+            if(current.getCurrentResource(t) == nextCell.getCurrentResource(t))
+            {
+                if(Math.abs( row - current.row ) < cellDist)
+                {
+                    nextCell = current;
+                    cellDist = Math.abs( row - current.row );
+                }
+            }
+            if(current.getCurrentResource(t) > nextCell.getCurrentResource(t))
+            {
+                nextCell = current;
+                cellDist = Math.abs( row - current.row );
+            }
+        }
+        // check down
+        for (int i = 1; i <= vision; i++)
+        {
+            int nextRow = (row - vision + NUM_ROWS + i) % NUM_ROWS;
+            current = landscape.getCellAt(nextRow, col);
 
-         if(current.getCurrentResource(t) == nextCell.getCurrentResource(t))
-         {
-             if(Math.abs( row - current.row ) < cellDist)
-             {
-                 nextCell = current;
-                 cellDist = Math.abs( row - current.row );
-             }
-         }
-         if(current.getCurrentResource(t) > nextCell.getCurrentResource(t))
-         {
-             nextCell = current;
-             cellDist = Math.abs( row - current.row );
-         }
-     }
-     // check down
-     for (int i = 1; i <= vision; i++)
-     {
-         int nextRow = (row - vision + NUM_ROWS + i) % NUM_ROWS;
-         current = landscape.getCellAt(nextRow, col);
+            if(current.getOccupied() == true)
+            {
+                continue;
+            }
 
-         if(current.getOccupied() == true)
-         {
-             continue;
-         }
+            if(current.getCurrentResource(t) == nextCell.getCurrentResource(t))
+            {
+                if(Math.abs( row - current.row ) < cellDist)
+                {
+                    nextCell = current;
+                    cellDist = =Math.abs( row - current.row );
+                }
+            }
+            if(current.getCurrentResource(t) > nextCell.getCurrentResource(t))
+            {
+                nextCell = current;
+                cellDist = Math.abs( row - current.row );
+            }
+        }
+        // check left
+        for (int i = 1; i <= vision; i++)
+        {
+            int nextCol = (col - vision + NUM_COLS - i) % NUM_COLS;
+            current = landscape.getCellAt(row, nextCol);
 
-         if(current.getCurrentResource(t) == nextCell.getCurrentResource(t))
-         {
-             if(Math.abs( row - current.row ) < cellDist)
-             {
-                 nextCell = current;
-                 cellDist = Math.abs( row - current.row );
-             }
-         }
-         if(current.getCurrentResource(t) > nextCell.getCurrentResource(t))
-         {
-             nextCell = current;
-             cellDist = Math.abs( row - current.row );
-         }
-     }
-     // check left
-     for (int i = 1; i <= vision; i++)
-     {
-         int nextCol = (col - vision + NUM_COLS - i) % NUM_COLS;
-         current = landscape.getCellAt(row, nextCol);
+            if(current.getOccupied() == true)
+            {
+                continue;
+            }
 
-         if(current.getOccupied() == true)
-         {
-             continue;
-         }
+            if(current.getCurrentResource(t) == nextCell.getCurrentResource(t))
+            {
+                if(Math.abs( col - current.col ) < cellDist)
+                {
+                    nextCell = current;
+                    cellDist = Math.abs( col - current.col );
+                }
+            }
+            if(current.getCurrentResource(t) > nextCell.getCurrentResource(t))
+            {
+                nextCell = current;
+                cellDist = Math.abs( col - current.col );
+            }
+        }
+        // check right
+        for (int i = 1; i <= vision; i++)
+        {
+            int nextCol = (col - vision + NUM_COLS + i) % NUM_COLS;
+            current = landscape.getCellAt(row, nextCol);
 
-         if(current.getCurrentResource(t) == nextCell.getCurrentResource(t))
-         {
-             if(Math.abs( col - current.col ) < cellDist)
-             {
-                 nextCell = current;
-                 cellDist = Math.abs( col - current.col );
-             }
-         }
-         if(current.getCurrentResource(t) > nextCell.getCurrentResource(t))
-         {
-             nextCell = current;
-             cellDist = Math.abs( col - current.col );
-         }
-     }
-     // check right
-     for (int i = 1; i <= vision; i++)
-     {
-         int nextCol = (col - vision + NUM_COLS + i) % NUM_COLS;
-         current = landscape.getCellAt(row, nextCol);
+            if(current.getOccupied() == true)
+            {
+                continue;
+            }
 
-         if(current.getOccupied() == true)
-         {
-             continue;
-         }
+            if(current.getCurrentResource(t) == nextCell.getCurrentResource(t))
+            {
+                if(Math.abs( col - current.col ) < cellDist)
+                {
+                    nextCell = current;
+                    cellDist = Math.abs( col - current.col );
+                }
+            }
+            if(current.getCurrentResource(t) > nextCell.getCurrentResource(t))
+            {
+                nextCell = current;
+                cellDist = Math.abs( col - current.col );
+            }
+        }
+        if (nextCell == null)
+        {
+            Cell original = landscape.getCellAt(this.row, this.col);
+            return original;
+        }
 
-         if(current.getCurrentResource(t) == nextCell.getCurrentResource(t))
-         {
-             if(Math.abs( col - current.col ) < cellDist)
-             {
-                 nextCell = current;
-                 cellDist = Math.abs( col - current.col );
-             }
-         }
-         if(current.getCurrentResource(t) > nextCell.getCurrentResource(t))
-         {
-             nextCell = current;
-             cellDist = Math.abs( col - current.col );
-         }
-     }
-     if (nextCell == null)
-     {
-         Cell original = landscape.getCellAt(this.row, this.col);
-         return original;
-     }
-
-     return nextCell;
- }
+        return nextCell;
+    }
 }
